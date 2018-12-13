0c4553d6167211e7da0971039410d54c77bbfc6e
vineeth.vijayan@lunatech.nl <vineeth.vijayan@lunatech.nl>
2018-12-13 16:29:41 +0100

    Dynamodb example added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # AWS-Lambda-Examples
+
+## Lambda examples to try out
+
+- Simple lambda execution
+- Lambda with API Gateway
+- Lambda with s3 as trigger
+- Lambda with s3 data processing
+- Lambda with SNS as trigger and pubish to topic
+- Lambda w
---
+++ lambda-dynamo_db/lambda_function.py
@@ -0,0 +1,23 @@
+import time
+from datetime import datetime
+import boto3
+
+DDB= boto3.resource('dynamodb').Table('logtime')
+
+def lambda_handler(event, context):
+    now = datetime.utcnow()
+    timestamp = int(time.mktime(now.timetuple()))
+
+    item = {
+        'Item': {
+            'timestamp': timestamp,
+            'datetime' : now.isoformat(),
+            'message' : "Test for added column"
+        }
+
+    }
+    print("Item: %s" % item)
+
+    DDB.put_item(**item)
+
+    return item
